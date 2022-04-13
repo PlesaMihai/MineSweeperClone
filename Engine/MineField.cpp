6f7eb0ba57e7007a4a91534c98464021d8784c97
Mihai <plesa.mihai01@gmail.com>
2022-04-13 12:31:13 +0300

    Add losing codition Add draw numbers of neighbor bombs

--- Engine/MineField.cpp
+++ Engine/MineField.cpp
@@ -3,6 +3,7 @@
 #include <random>
 #include "Vei2.h"
 #include "SpriteCodex.h"
+#include <algorithm>
 
 void MineField::Tile::SpawnMine()
 {
@@ -15,28 +16,67 @@ bool MineField::Tile::HasMine() const
 	return hasMine;
 }
 
-void MineField::Tile::Draw(const Vei2 & screenPos, Graphics & gfx) const
+void MineField::Tile::Draw(const Vei2& screenPos, bool fucked, Graphics& gfx) const
 {
-	switch (state)
+	if (!fucked)
 	{
-	case MineField::Tile::State::Hidden:
-		SpriteCodex::DrawTileButton(screenPos, gfx);
-		break;
-	case MineField::Tile::State::Flagged:
-		SpriteCodex::DrawTileButton(screenPos, gfx);
-		SpriteCodex::DrawTileFlag(screenPos, gfx);
-		break;
-	case MineField::Tile::State::Revealed:
-		if (!hasMine)
+		switch (state)
 		{
-			SpriteCodex::DrawTile0(screenPos, gfx);
+		case MineField::Tile::State::Hidden:
+			SpriteCodex::DrawTileButton(screenPos, gfx);
+			break;
+		case MineField::Tile::State::Flagged:
+			SpriteCodex::DrawTileButton(screenPos, gfx);
+			SpriteCodex::DrawTileFlag(screenPos, gfx);
+			break;
+		case MineField::Tile::State::Revealed:
+			if (!hasMine)
+			{
+				SpriteCodex::DrawTileNumber(screenPos, nNeighborMemes, gfx);
+			}
+			else {
+				SpriteCodex::DrawTileBomb(screenPos, gfx);
+			}
+			break;
+		default:
+			break;
 		}
-		else {
-			SpriteCodex::DrawTileBomb(screenPos, gfx);
+	}
+	else {
+		switch (state)
+		{
+		case MineField::Tile::State::Hidden:
+			if (HasMine())
+			{
+				SpriteCodex::DrawTileBomb(screenPos, gfx);
+			}
+			else {
+				SpriteCodex::DrawTileButton(screenPos, gfx);
+			}
+			break;
+		case MineField::Tile::State::Flagged:
+			if (HasMine())
+			{
+				SpriteCodex::DrawTileBomb(screenPos, gfx);
+				SpriteCodex::DrawTileFlag(screenPos, gfx);
+			}
+			else {
+				SpriteCodex::DrawTileBomb(screenPos, gfx);
+				SpriteCodex::DrawTileCross(screenPos, gfx);
+			}
+			break;
+		case MineField::Tile::State::Revealed:
+			if (!hasMine)
+			{
+				SpriteCodex::DrawTileNumber(screenPos, nNeighborMemes, gfx);
+			}
+			else {
+				SpriteCodex::DrawTileBombRed(screenPos, gfx);
+			}
+			break;
+		default:
+			break;
 		}
-		break;
-	default:
-		break;
 	}
 }
 
@@ -49,6 +89,29 @@ void MineField::Tile::Reveal()
 bool MineField::Tile::IsRevealed() const
 {
 	return state == State::Revealed;
+}
+
+void MineField::Tile::ToggleFlag()
+{
+	assert(!IsRevealed());
+	if (state == State::Hidden)
+	{
+		state = State::Flagged;
+	}
+	else {
+		state = State::Hidden;
+	}
+}
+
+bool MineField::Tile::IsFlagged() const
+{
+	return state == State::Flagged;
+}
+
+void MineField::Tile::SetNeighborMemeCount(int memeCount)
+{
+	assert(nNeighborMemes == -1);
+	nNeighborMemes = memeCount;
 }
 
 MineField::MineField(int nMines)
@@ -69,6 +132,15 @@ MineField::MineField(int nMines)
 
 		TileAt(spawnPos).SpawnMine();
 	}
+
+	for (Vei2 gridPos = { 0,0 }; gridPos.y < height; gridPos.y++)
+	{
+		for (gridPos.x = 0; gridPos.x < width; gridPos.x++)
+		{
+			TileAt(gridPos).SetNeighborMemeCount(CountNeighborMemes(gridPos));
+		}
+
+	}
 }
 
 void MineField::Draw(Graphics & gfx) const
@@ -78,7 +150,7 @@ void MineField::Draw(Graphics & gfx) const
 	{
 		for (gridPos.x = 0; gridPos.x < width; gridPos.x++)
 		{
-			TileAt(gridPos).Draw(gridPos * SpriteCodex::tileSize, gfx);
+			TileAt(gridPos).Draw(gridPos * SpriteCodex::tileSize, isFucked, gfx);
 		}
 
 	}
@@ -91,12 +163,34 @@ RectI MineField::GetRect() const
 
 void MineField::OnRevealClick(const Vei2 & screenPos)
 {
-	const Vei2 gridPos = ScreenToGrid(screenPos);
-	assert(gridPos.x >= 0 && gridPos.x < width && gridPos.y >= 0 && gridPos.y < height);
-	Tile& tile = TileAt(gridPos);
-	if (!tile.IsRevealed())
+	if(!isFucked)
 	{
-		tile.Reveal();
+		const Vei2 gridPos = ScreenToGrid(screenPos);
+		assert(gridPos.x >= 0 && gridPos.x < width&& gridPos.y >= 0 && gridPos.y < height);
+		Tile& tile = TileAt(gridPos);
+		if (!tile.IsRevealed() && !tile.IsFlagged())
+		{
+			tile.Reveal();
+			if (tile.HasMine())
+			{
+				isFucked = true;
+			}
+		}
+	}
+
+}
+
+void MineField::OnFlagClick(const Vei2& screenPos)
+{
+	if (!isFucked)
+	{
+		const Vei2 gridPos = ScreenToGrid(screenPos);
+		assert(gridPos.x >= 0 && gridPos.x < width&& gridPos.y >= 0 && gridPos.y < height);
+		Tile& tile = TileAt(gridPos);
+		if (!tile.IsRevealed())
+		{
+			tile.ToggleFlag();
+		}
 	}
 }
 
@@ -113,4 +207,26 @@ const MineField::Tile & MineField::TileAt(const Vei2 & gridPos) const
 Vei2 MineField::ScreenToGrid(const Vei2 & screenPos)
 {
 	return screenPos / SpriteCodex::tileSize;
+}
+
+int MineField::CountNeighborMemes(const Vei2& gridPos)
+{
+	const int xStart = std::max(0, gridPos.x - 1);
+	const int yStart = std::max(0, gridPos.y - 1);
+	const int xEnd = std::min(width - 1, gridPos.x + 1);
+	const int yEnd = std::min(height - 1, gridPos.y + 1);
+
+	int count = 0;
+	for (Vei2 gridPos = { xStart, yStart }; gridPos.y <= yEnd; gridPos.y++)
+	{
+		for (gridPos.x = xStart; gridPos.x <= xEnd; gridPos.x++)
+		{
+			if (TileAt(gridPos).HasMine())
+			{
+				count++;
+			}
+		}
+	}
+
+	return count;
 }
